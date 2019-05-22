237fe432af11840055fe29f560b607bd0b106361
Théo Issarni <issarnitheo@gmail.com>
2019-05-22 21:27:07 +0200

    misc: getting started finished

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import * as bodyParser from 'body-parser';
+import { createExpressServer } from 'routing-controllers';
+import { UserController } from './controller/UserController';
+import {
+  PokemonSpeciesController,
+} from './controller/PokemonSpeciesController';
+import { TypeController } from './controller/TypeController';
+
+createConnection().then(async (connection) => {
+
+  // create express app
+  const app = createExpressServer({
+    controllers: [UserController, PokemonSpeciesController, TypeController],
+  });
+  app.use(bodyParser.json());
+
+  // setup express app here
+  // ...
+
+  // start express server
+  app.listen(3000);
+
+  console.log(`Express server has started on port 3000.
+Open http://localhost:3000/users to see results`);
+
+}).catch(error => console.log(error));

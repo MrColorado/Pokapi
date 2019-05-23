306923dbcfad3a026ec237ca4c54ff9b4567085c
Théo Issarni <issarnitheo@gmail.com>
2019-05-23 21:24:24 +0200

    misc: update package.json

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: yarn install; yarn start
-
+web: yarn install; yarn fixtures fixtures/Types.yml; yarn fixtures fixtures/PokemonSpecies.yml; yarn start
--- src/index.ts
+++ src/index.ts
@@ -33,11 +33,8 @@ createConnection().then(async (connection) => {
 
   app.use(bodyParser.json());
 
-  // setup express app here
-  // ...
-
   // start express server
-  app.listen(3000);
+  app.listen(process.env.PORT || 3000);
 
   console.log(`Express server has started on port 3000.
 Open http://localhost:3000/users to see results`);

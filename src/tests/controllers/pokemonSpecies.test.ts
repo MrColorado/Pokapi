a7820e4da4a8b20d87b812b87f840b3e0b324178
Théo Issarni <issarnitheo@gmail.com>
2019-05-23 14:31:46 +0200

    testSuite: add few more tests

--- src/tests/controllers/pokemonSpecies.test.ts
+++ src/tests/controllers/pokemonSpecies.test.ts
@@ -1,25 +1,25 @@
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
-import { app, types } from '../setups';
+import { app, pokemonSpecies } from '../setups';
 import { it, describe } from 'mocha';
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 
-describe('TypeController GET', () => {
+describe('PokemonSpeciesController GET', () => {
   it('Should respond with 200', async () => {
-    const res = await chai.request(app).get('/types');
+    const res = await chai.request(app).get('/pokemons');
     expect(res).to.have.status(200);
   });
 
   it('Should return existing types', async () => {
-    const res = await chai.request(app).get('/types');
+    const res = await chai.request(app).get('/pokemons');
     expect(res).to.be.json;
     expect(res.body).to.have.length.above(0);
-    expect(res.body).to.have.length(types.length);
-    const dbIds = types.map(list => list.name);
-    const responseIds = res.body.map(list => list.name);
+    expect(res.body).to.have.length(pokemonSpecies.length);
+    const dbIds = pokemonSpecies.map(list => list.id);
+    const responseIds = res.body.map(list => list.id);
     expect(responseIds).to.have.members(dbIds);
   });
 });
